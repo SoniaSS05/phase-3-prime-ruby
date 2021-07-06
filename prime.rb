4dcef16240c1db73414307011b3dee406a5ce576
Sonia <serrano_sonia@hotmail.com>
2021-07-05 21:25:56 -0400

    Completed Assignment

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+    if number<= 1
+        return false
+    else
+        for ct in (2..number-1)
+            if (number % ct == 0 && number != ct)
+              return false
+            end
+        end
+        return true
+    end
+end
